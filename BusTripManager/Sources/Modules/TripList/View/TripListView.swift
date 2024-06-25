e31fb194bb218c470d6f594c24db3152ea1c700e
Raul Montoya <rmontoya@offshoretech.net>
2024-06-25 14:47:59 +0200

    Fix TripListView preview

--- BusTripManager/Sources/Modules/TripList/View/TripListView.swift
+++ BusTripManager/Sources/Modules/TripList/View/TripListView.swift
@@ -23,5 +23,25 @@ struct TripListView: View {
 }
 
 #Preview {
-    TripListView(trips: TripListViewModel(tripService: TripService()).trips)
+    TripListView(trips: 
+                    [Trip(driverName: "Driver1",
+                          status: "ongoing",
+                          route: "sdq{Fc}iLj@zR|W~TryCzvC??do@jkKeiDxjIccLhiFqiE`uJqe@rlCy~B`t@sK|i@",
+                          startTime: "2018-12-19T08:00:00.000Z",
+                          origin: Location(address: "originLocation1", point: Point(latitude: 41.38074, longitude: 2.18594)),
+                          description: "trip1 description",
+                          destination: Location(address: "destinationLocation1", point: Point(latitude: 41.49958, longitude: 1.90307)),
+                          stops: [Stop(point: Point(latitude: 41.35179, longitude: 2.15494), id: 1)],
+                          endTime: "2018-12-19T10:00:00.000Z"),
+                     Trip(driverName: "Driver2",
+                          status: "scheduled",
+                          route: "",
+                          startTime: "2018-12-18T08:00:00.000Z",
+                          origin: Location(address: "originLocation2", point: Point(latitude: 0.0, longitude: 0.0)),
+                          description: "trip2 description",
+                          destination: Location(address: "destinationLocation2", point: Point(latitude: 0.0, longitude: 0.0)),
+                          stops: [],
+                          endTime: "2018-12-18T09:00:00.000Z")
+                    ]
+                 )
 }
