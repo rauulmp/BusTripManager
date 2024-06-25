9855c7a935ab3d5d52d42c9d333f0bcda5b48f31
Raul Montoya <rmontoya@offshoretech.net>
2024-06-25 12:36:15 +0200

    Refactor MockTripService

--- BusTripManager/Tests/Mocks/MockTripService.swift
+++ BusTripManager/Tests/Mocks/MockTripService.swift
@@ -21,14 +21,41 @@ class MockTripService: TripServiceProtocol {
     }
     
     func decodePolyline(encodedPolyline: String) -> [LocationCoordinate2D]? {
+        if !encodedPolyline.isEmpty {
+            return Polyline(encodedPolyline: encodedPolyline).coordinates
+        }
         return getMockPolylines()
     }
     
     private func getMockTrips() -> [Trip] {
         let mockTrips = [
-            Trip(driverName: "Driver1", status: "ongoing", route: "routee", startTime: "start time", origin: Location(address: "originLocation1", point: Point(latitude: 0.0, longitude: 0.0)), description: "trip1 desc", destination: Location(address: "destinationLocation1", point: Point(latitude: 0.0, longitude: 0.0)), stops: [], endTime: "endTime"),
-            Trip(driverName: "Driver2", status: "ongoing", route: "routee", startTime: "start time", origin: Location(address: "originLocation2", point: Point(latitude: 0.0, longitude: 0.0)), description: "trip1 desc", destination: Location(address: "destinationLocation2", point: Point(latitude: 0.0, longitude: 0.0)), stops: [], endTime: "endTime"),
-            Trip(driverName: "Driver3", status: "ongoing", route: "routee", startTime: "start time", origin: Location(address: "originLocation3", point: Point(latitude: 0.0, longitude: 0.0)), description: "trip1 desc", destination: Location(address: "destinationLocation3", point: Point(latitude: 0.0, longitude: 0.0)), stops: [], endTime: "endTime")
+            Trip(driverName: "Driver1",
+                 status: "ongoing",
+                 route: "sdq{Fc}iLj@zR|W~TryCzvC??do@jkKeiDxjIccLhiFqiE`uJqe@rlCy~B`t@sK|i@",
+                 startTime: "2018-12-19T08:00:00.000Z",
+                 origin: Location(address: "originLocation1", point: Point(latitude: 41.38074, longitude: 2.18594)),
+                 description: "trip1 description",
+                 destination: Location(address: "destinationLocation1", point: Point(latitude: 41.49958, longitude: 1.90307)),
+                 stops: [Stop(point: Point(latitude: 41.35179, longitude: 2.15494), id: 1)],
+                 endTime: "2018-12-19T10:00:00.000Z"),
+            Trip(driverName: "Driver2", 
+                 status: "scheduled",
+                 route: "",
+                 startTime: "2018-12-18T08:00:00.000Z",
+                 origin: Location(address: "originLocation2", point: Point(latitude: 0.0, longitude: 0.0)),
+                 description: "trip2 description",
+                 destination: Location(address: "destinationLocation2", point: Point(latitude: 0.0, longitude: 0.0)),
+                 stops: [],
+                 endTime: "2018-12-18T09:00:00.000Z"),
+            Trip(driverName: "Driver3", 
+                 status: "cancelled",
+                 route: "",
+                 startTime: "2018-11-18T19:00:00.000Z",
+                 origin: Location(address: "originLocation3", point: Point(latitude: 0.0, longitude: 0.0)),
+                 description: "trip3 description",
+                 destination: Location(address: "destinationLocation3", point: Point(latitude: 0.0, longitude: 0.0)),
+                 stops: [],
+                 endTime: "2018-11-18T09:00:00.000Z")
         ]
         return mockTrips
     }
